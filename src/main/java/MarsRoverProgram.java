f0ca7d856d320b2b22c14b56bc5ad4473c9cce72
Vincent <va543567@gmail.com>
2024-11-14 11:45:05 +0000

    feat: initial Plateau class in the MarsRoverProgram

---
+++ src/main/java/MarsRoverProgram.java
@@ -0,0 +1,14 @@
+import model.Plateau;
+
+public class MarsRoverProgram {
+    private Plateau plateau;
+
+    public void init(int plateauPositionX, int plateauPositionY, int numOfRover) {
+
+        plateau = new Plateau(plateauPositionX, plateauPositionY);
+
+        // TODO: Create # of Rover to be land
+
+    }
+
+}
---
+++ src/main/java/model/Plateau.java
@@ -0,0 +1,20 @@
+package model;
+
+public class Plateau {
+    private final int positionX;
+    private final int positionY;
+
+    public Plateau(int positionX, int positionY) {
+        this.positionX = positionX;
+        this.positionY = positionY;
+    }
+
+    public int getPositionX() {
+        return positionX;
+    }
+
+    public int getPositionY() {
+        return positionY;
+    }
+
+}
