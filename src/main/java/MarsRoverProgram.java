46a3e9145c1e64b18dbfe1076a196ade0eb01535
Vincent <va543567@gmail.com>
2024-11-14 13:31:40 +0000

    feat: add Rover model

--- src/main/java/MarsRoverProgram.java
+++ src/main/java/MarsRoverProgram.java
@@ -1,13 +1,23 @@
 import model.Plateau;
+import model.Rover;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class MarsRoverProgram {
     private Plateau plateau;
+    private List<Rover> roverList = new LinkedList<>();
 
     public void init(int plateauPositionX, int plateauPositionY, int numOfRover) {
 
         plateau = new Plateau(plateauPositionX, plateauPositionY);
 
-        // TODO: Create # of Rover to be land
+        for (int i = 0; i < numOfRover; i++) {
+            String roverName = "Rover " + (i+1);
+            Rover rover = new Rover(roverName);
+
+            roverList.add(rover);
+        }
 
     }
 
---
+++ src/main/java/model/Rover.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Rover {
+    private String name;
+
+    public Rover(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
