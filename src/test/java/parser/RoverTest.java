0f1ed150299bb4aa360e155ca7f5dad0501f1711
Vincent <va543567@gmail.com>
2024-11-14 18:27:17 +0000

    feat: add rover move forward and backward test case

--- src/test/java/parser/RoverTest.java
+++ src/test/java/parser/RoverTest.java
@@ -55,9 +55,6 @@ public class RoverTest {
 
     }
 
-
-
-
     @Test
     void testRoverRotate_rightFromN2E() {
         Position position = new Position(1, 2, Direction.N);
@@ -99,7 +96,7 @@ public class RoverTest {
     }
 
     @Test
-    void testRoverMove_forward() {
+    void testRoverMove_forwardFacingN() {
         Position position = new Position(1, 2, Direction.N);
         Rover rover = new Rover(1, "Rover", position);
 
@@ -111,7 +108,55 @@ public class RoverTest {
     }
 
     @Test
-    void testRoverMove_backward() {
+    void testRoverMove_forwardFacingN_2steps() {
+        Position position = new Position(1, 2, Direction.N);
+        Rover rover = new Rover(1, "Rover", position);
+
+        rover.move(2);
+
+        assertEquals(1, rover.getPosition().getX());
+        assertEquals(4, rover.getPosition().getY());
+
+    }
+
+    @Test
+    void testRoverMove_forwardFacingE() {
+        Position position = new Position(1, 2, Direction.E);
+        Rover rover = new Rover(1, "Rover", position);
+
+        rover.move(1);
+
+        assertEquals(2, rover.getPosition().getX());
+        assertEquals(2, rover.getPosition().getY());
+
+    }
+
+    @Test
+    void testRoverMove_forwardFacingS() {
+        Position position = new Position(1, 2, Direction.S);
+        Rover rover = new Rover(1, "Rover", position);
+
+        rover.move(1);
+
+        assertEquals(1, rover.getPosition().getX());
+        assertEquals(1, rover.getPosition().getY());
+
+    }
+
+    @Test
+    void testRoverMove_forwardFacingW() {
+        Position position = new Position(1, 2, Direction.W);
+        Rover rover = new Rover(1, "Rover", position);
+
+        rover.move(1);
+
+        assertEquals(0, rover.getPosition().getX());
+        assertEquals(2, rover.getPosition().getY());
+
+    }
+
+    @Test
+    void testRoverMove_backwardFacingN() {
         Position position = new Position(1, 2, Direction.N);
         Rover rover = new Rover(1, "Rover", position);
 
@@ -119,6 +164,50 @@ public class RoverTest {
 
         assertEquals(1, rover.getPosition().getX());
         assertEquals(1, rover.getPosition().getY());
+    }
+
+    @Test
+    void testRoverMove_backwardFacingN_2steps() {
+        Position position = new Position(1, 2, Direction.N);
+        Rover rover = new Rover(1, "Rover", position);
+
+        rover.move(-1);
+
+        assertEquals(1, rover.getPosition().getX());
+        assertEquals(3, rover.getPosition().getY());
+    }
+
+    @Test
+    void testRoverMove_backwardFacingE() {
+        Position position = new Position(1, 2, Direction.E);
+        Rover rover = new Rover(1, "Rover", position);
+
+        rover.move(-1);
+
+        assertEquals(0, rover.getPosition().getX());
+        assertEquals(2, rover.getPosition().getY());
+    }
+
+    @Test
+    void testRoverMove_backwardFacingS() {
+        Position position = new Position(1, 2, Direction.S);
+        Rover rover = new Rover(1, "Rover", position);
+
+        rover.move(-1);
+
+        assertEquals(1, rover.getPosition().getX());
+        assertEquals(3, rover.getPosition().getY());
+    }
+
+    @Test
+    void testRoverMove_backwardFacingW() {
+        Position position = new Position(1, 2, Direction.W);
+        Rover rover = new Rover(1, "Rover", position);
+
+        rover.move(-1);
+
+        assertEquals(1, rover.getPosition().getX());
+        assertEquals(3, rover.getPosition().getY());
     }
 
 }
